78c82b36c62acc755c2728e9bfb78f63f23f0908
Hunain Pervaiz <106392698+SardarHunain@users.noreply.github.com>
2023-05-05 21:30:35 +0500

    Add files via upload

---
+++ PedestrianTrackerApp.py
@@ -0,0 +1,54 @@
+import cv2
+#image
+img = "401_Gridlock.JPG"
+
+video = cv2.VideoCapture('carVideo.mp4')
+
+#creat opencv image
+img = cv2.imread(img)
+
+#convert to grey scale
+blackNwhite = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#pre trained classifier
+classifierFile = "cars.xml"
+
+#create car classifier
+car_tracker = cv2.CascadeClassifier(classifierFile)
+
+#run forever until car stops
+while True:
+    #read the current frame
+    (readSuccessful, frame) = video.read()
+
+    if readSuccessful:
+        #must convert to grey
+        greyscaledFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    else:
+        break
+
+    #detect cars
+    cars = car_tracker.detectMultiScale(greyscaledFrame)
+
+    # draw rectangles around cars
+    for (x, y, w, z) in cars:
+        cv2.rectangle(frame, (x, y), (x + w, y + z), (0, 255, 255), 2)
+
+    cv2.imshow('Car Detector',frame)
+    cv2.waitKey(1)
+
+
+#Detect Cars
+cars = car_tracker.detectMultiScale(blackNwhite)
+
+#draw rectangles around cars
+for(x,y,w,z) in cars:
+    cv2.rectangle(img,(x,y),(x+w,y+z),(0,0,255),2)
+
+#display image with faces spotted
+cv2.imshow("clever programmer car detector",img)
+
+
+#dont autoclose
+cv2.waitKey()
+
